c3ef6ae3e5968acb9c00113709221af238026b5a
Brian Pan <brianpanzifan@gmail.com>
2022-04-14 00:07:12 -0400

    DB config & connection done

--- backend/server.js
+++ backend/server.js
@@ -4,21 +4,10 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 const PORT = process.env.PORT || 5000;
 
 //connect MongoDB
-// mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false
-// });
-
-// const db = mongoose.connection;
-// db.on("error", console.error.bind(console, "connection error:"));
-// db.once("open", () => {
-//     console.log("Database connected");
-// });
+const connectDB = require("./config/db");
+connectDB();
 
 const app = express();
-
 app.use(express.json);
 app.use(express.urlencoded({ extended: false }));
 
@@ -36,3 +25,16 @@ app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}:`);
 });
+
+// mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false
+// });
+
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", () => {
+//     console.log("Database connected");
+// });
